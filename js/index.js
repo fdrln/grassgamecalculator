9a4907a637330f7e6f936f875c7c6556268c7b31
Tobias Federlein <tobifdrln@gmail.com>
2024-08-11 20:15:39 +0200

    Saving player names to array

--- js/index.js
+++ js/index.js
@@ -1,15 +1,26 @@
 let playerAmount;
+let playerNames = [];
+let playerNameInput;
 
 function createPlayerNameInputs() {
-  playerNames.innerHTML = "";
+  playerNameInputs.innerHTML = "";
   playerAmount = document.getElementById("playerSelect").value;
 
   for (i = 0; i < playerAmount; i++) {
-    var playerNameInput = document.createElement("input");
+    playerNameInput = document.createElement("input");
     playerNameInput.type = "text";
     playerNameInput.classList.add("player-name");
     playerNameInput.placeholder = `Spieler ${i + 1} Name`;
-    playerNameInput.id = `spieler${i + 1}`;
-    playerNames.appendChild(playerNameInput);
+    playerNameInput.id = `player${i + 1}`;
+    playerNameInputs.appendChild(playerNameInput);
   }
+}
+
+function savePlayerNames() {
+  const playerInputs = document.querySelectorAll(".player-name");
+  playerNames = [];
+  playerInputs.forEach((input) => {
+    playerNames.push(input.value);
+    console.log("Player names:", playerNames);
+  });
 }
